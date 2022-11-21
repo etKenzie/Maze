5ad57229dd2b665df3717917a814af2839575cbe
kenzie.evan <kenzie.evan@sunterra.id>
2022-11-21 17:46:07 -0500

    Transfer Data between title and State Generating

--- app/src/main/java/edu/wm/cs/cs301/amazebykenzieevan/MainActivity.java
+++ app/src/main/java/edu/wm/cs/cs301/amazebykenzieevan/MainActivity.java
@@ -1,15 +1,30 @@
 package edu.wm.cs.cs301.amazebykenzieevan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    private TextView textLevel;
+    private SeekBar seekbarLevel;
+
+    private Button buttonExplore;
+    private Button buttonRevisit;
+
+    private Boolean roomState;
+    private String mazeGenerator;
+    private String mazeDriver;
+    private String Level;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,13 +45,59 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         SpinnerMazeDriver.setAdapter(adapter2);
         SpinnerMazeDriver.setOnItemSelectedListener(this);
 
-        String mazeGenerator = SpinnerMazeGenerator.getItemAtPosition(SpinnerMazeGenerator.getSelectedItemPosition()).toString();
-        String mazeDriver = SpinnerMazeDriver.getItemAtPosition(SpinnerMazeDriver.getSelectedItemPosition()).toString();
 
 
+        textLevel = (TextView) findViewById(R.id.textLevel);
+        seekbarLevel = (SeekBar) findViewById(R.id.seekbarLevel);
 
+        seekbarLevel.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                textLevel.setText("Level " + progress);
+                // Convert TextLevel to String
+                Level = textLevel.getText().toString();
+            }
 
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
 
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        // Buttons to Transition to new activity
+        buttonExplore = (Button) findViewById(R.id.buttonExplore);
+        buttonRevisit = (Button) findViewById(R.id.buttonRevisit);
+
+        buttonExplore.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Strings that keep which Maze Generator and Driver to Use
+                mazeGenerator = SpinnerMazeGenerator.getItemAtPosition(SpinnerMazeGenerator.getSelectedItemPosition()).toString();
+                mazeDriver = SpinnerMazeDriver.getItemAtPosition(SpinnerMazeDriver.getSelectedItemPosition()).toString();
+
+                // Create Boolean to check if room should be on or off.
+                Switch switchRoom = (Switch) findViewById(R.id.switchRoom);
+                roomState = switchRoom.isChecked();
+
+                openStateGenerating();
+            }
+        });
+
+    }
+
+    public void openStateGenerating() {
+        Intent intent = new Intent(this, StateGenerating.class);
+        intent.putExtra("mazeGenerator", mazeGenerator);
+        intent.putExtra("mazeDriver", mazeDriver);
+        intent.putExtra("roomState", roomState);
+        intent.putExtra("Level", Level);
+
+        startActivity(intent);
     }
 
     @Override
--- app/src/main/java/edu/wm/cs/cs301/amazebykenzieevan/StateGenerating.java
+++ app/src/main/java/edu/wm/cs/cs301/amazebykenzieevan/StateGenerating.java
@@ -1,5 +1,6 @@
 package edu.wm.cs.cs301.amazebykenzieevan;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,5 +11,16 @@ public class StateGenerating extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_state_generating);
+
+        // Retrieve Previous Values
+        Intent intent = getIntent();
+
+        String Level = intent.getStringExtra("Level");
+        String mazeGenerator = intent.getStringExtra("mazeGenerator");
+        String mazeDriver = intent.getStringExtra("mazeDriver");
+        Boolean roomState = intent.getExtras().getBoolean("roomState");
+
+
     }
+
 }
